f2b71a43e13c7e43da55f3f856db842f1a17b258
Greg Tatum <tatum.creative@gmail.com>
2016-06-13 06:30:38 -0500

    Add support for WebGL 2

--- vao.js
+++ vao.js
@@ -3,9 +3,18 @@
 var createVAONative = require("./lib/vao-native.js")
 var createVAOEmulated = require("./lib/vao-emulated.js")
 
+function ExtensionShim (gl) {
+  this.bindVertexArrayOES = gl.bindVertexArray.bind(gl)
+  this.createVertexArrayOES = gl.createVertexArray.bind(gl)
+  this.deleteVertexArrayOES = gl.deleteVertexArray.bind(gl)
+}
+
 function createVAO(gl, attributes, elements, elementsType) {
-  var ext = gl.getExtension('OES_vertex_array_object')
+  var ext = gl.createVertexArray
+    ? new ExtensionShim(gl)
+    : gl.getExtension('OES_vertex_array_object')
   var vao
+
   if(ext) {
     vao = createVAONative(gl, ext)
   } else {
